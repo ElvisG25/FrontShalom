c855faea96689554ea997ae12a44d0deb6a3bff3
ElvisG25 <elvisgonzales86@gmail.com>
2024-03-21 14:34:54 -0500

    comentarios estaticos

--- src/app/Services/servicios/servicios.service.ts
+++ src/app/Services/servicios/servicios.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { collectionData } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore/firebase';
+import { addDoc, collection } from '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
 
-  constructor() { }
+  // constructor(private fs:Firestore) { }
+  // EnviarComentario(){
+  //   let  comentarioCollection = collection(this.fs, 'Comentarios');
+  //   return collectionData(comentarioCollection,{idField:'id'})
+  // }
+  // AgregarComentario(desc: string){
+  //   let data = {description:desc};
+  //   let comentarioCollection = collection(this.fs,'Comentarios');
+  //   return addDoc(comentarioCollection,data);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Componentes/home/home.component';
 import { LoginComponent } from './Componentes/login/login.component';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { ServiciosService } from './Services/servicios/servicios.service';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA2zvc7hTa_gfXADYJjMprGvUquYUrtm6I",
+//   authDomain: "shalomrecepcioneseventos-a109f.firebaseapp.com",
+//   projectId: "shalomrecepcioneseventos-a109f",
+//   storageBucket: "shalomrecepcioneseventos-a109f.appspot.com",
+//   messagingSenderId: "86176467667",
+//   appId: "1:86176467667:web:4f0d27d32c3c000b81a226"
+// };
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +27,11 @@ import { LoginComponent } from './Componentes/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    // provideFirestore(() => getFirestore()),
   ],
-  providers: [],
+  providers: [ServiciosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
